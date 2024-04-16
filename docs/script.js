ef3348320e61855eaa42b74c066e5bc0c178437e
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2024-04-16 12:33:25 +0200

    remove docs

--- docs/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("click", (e) => {
-  if (e.target instanceof HTMLButtonElement && e.target.dataset["copy"]) {
-    return navigator?.clipboard?.writeText(e.target.dataset["copy"]);
-  }
-});
