eda36dca0045195febca8bb9908e78608faa55c5
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2024-04-20 17:32:48 +0200

    update models and exports

--- mod.mjs
+++ mod.mjs
@@ -1,5 +1,7 @@
 // export { SemanticData, I18nString } from './imports.mjs';
 export { IRI, UniformResourceName } from './deps.mjs';
+export { prefixes } from './lib/rdf/prefixes.mjs';
+export { LangString } from './lib/rdf/lang-string.mjs';
 export { Model } from './lib/model.mjs';
 export { Person } from './lib/models/person.mjs';
 // export { Product } from './models/product.mjs';
